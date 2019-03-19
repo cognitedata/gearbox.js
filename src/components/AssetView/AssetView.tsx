c0512f38f1ae703d666c0ccb802eae8c260c757a
Kamilla Svendsen <kamillasvendsen@gmail.com>
2019-03-19 14:46:59 +0100

    Moved test-files to components folder

--- src/components/AssetView/AssetView.tsx
+++ src/components/AssetView/AssetView.tsx
@@ -5,6 +5,20 @@ export interface AssetViewProps {
   asset: {
     id: number;
     name?: string;
+    path?: string[];
+    description?: string;
+    metadata?: {
+      ASSETSCOPENAME?: string;
+      DESCRIPTION?: string;
+      NAME?: string;
+      PARENTUID?: string;
+      SOURCE?: string;
+      SOURCEID?: string;
+      SOURCE_DB?: string;
+      SOURCE_TABLE?: string;
+      TYPE?: string;
+      UID?: string;
+    };
   };
   onClick?: (id: number) => void;
   onClose?: () => void;
