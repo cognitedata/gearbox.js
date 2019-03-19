f5e2b395b4e1b1fe729c36d007ecef6832e718af
f1cognite <fredrik.anfinsen@cognite.com>
2019-03-19 14:56:30 +0100

    Correct module root folder for jest

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js', 'tsx', 'json', 'node'],
   coverageDirectory: './coverage/',
   collectCoverage: true,
+  modulePaths: ['./src'],
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": "<rootDir>/node_modules/jest-css-modules-transform",
   }
