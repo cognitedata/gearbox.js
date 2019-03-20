4d0fa5d3c3465790a72a19c649fc36d43a856098
Roman <mr.traser@gmail.com>
2019-03-20 12:49:01 +0200

    Remove PropTypes section for each component in Storybook

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import TenantSelector from './TenantSelector/TenantSelector';
+import AssetView from './AssetView/AssetView';
+import EventPreview from './EventPreview/EventPreview';
+
+export { TenantSelector, AssetView, EventPreview };
