8e5bc30b3430f3fb5b24d455a8bbd98c781afd4d
Kamilla Svendsen <kamillasvendsen@gmail.com>
2019-03-25 13:46:20 +0100

    #14 add some references to components

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@
 export * from './components/AssetView/AssetView';
 export * from './components/TenantSelector/TenantSelector';
 export * from './components/EventPreview/EventPreview';
+export * from './components/AssetEventsPanel/AssetEventsPanel';
+export * from './components/AssetDetailsPanel/AssetDetailsPanel';
