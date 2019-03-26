bd94bc0b3ced3ac286cbf33bfb6ca8786a22a16a
Evan Charlton <evan.charlton@cognite.com>
2019-03-26 21:20:33 +0100

    Sanitize the incoming tenant
    Cognite project names are always [a-z0-9_-]+, so ensure that whenever
    the state is updated, the new value matches this pattern.
    
    Additionally, rename the helper function because sanitizeTests doesn't
    make sense to me.
    
    Fixes #48

--- src/__tests__/sanitize.test.ts
+++ src/__tests__/sanitize.test.ts
@@ -1,27 +1,27 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { sanitizeTests } from '../sanitize';
+import { sanitizeTenant } from '../sanitize';
 
 configure({ adapter: new Adapter() });
 
 describe('sanitize', () => {
   it('Handles correct tenants', () => {
-    expect(sanitizeTests('abc')).toEqual('abc');
+    expect(sanitizeTenant('abc')).toEqual('abc');
   });
 
   it('Handles uppercase tenants', () => {
-    expect(sanitizeTests('ABC')).toEqual('abc');
+    expect(sanitizeTenant('ABC')).toEqual('abc');
   });
 
   it('Handles tenants with numbers', () => {
-    expect(sanitizeTests('abc123')).toEqual('abc123');
+    expect(sanitizeTenant('abc123')).toEqual('abc123');
   });
 
   it('Handles tenants with special characters', () => {
-    expect(sanitizeTests('a b c')).toEqual('abc');
-    expect(sanitizeTests('a B c')).toEqual('abc');
-    expect(sanitizeTests('a_b_c')).toEqual('abc');
-    expect(sanitizeTests('a1b2c3')).toEqual('a1b2c3');
-    expect(sanitizeTests('Cognite AS')).toEqual('cogniteas');
+    expect(sanitizeTenant('a b c')).toEqual('abc');
+    expect(sanitizeTenant('a B c')).toEqual('abc');
+    expect(sanitizeTenant('a_b_c')).toEqual('abc');
+    expect(sanitizeTenant('a1b2c3')).toEqual('a1b2c3');
+    expect(sanitizeTenant('Cognite AS')).toEqual('cogniteas');
   });
 });
