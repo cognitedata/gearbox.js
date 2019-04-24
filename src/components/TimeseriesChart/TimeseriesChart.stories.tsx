2e814412c53877ecc48936f57aadc59b3b219093
Torbjørn Follan <torbjorn.follan@itera.no>
2019-04-24 10:37:26 +0200

    Add TImeseriesChart unit tests

--- src/components/TimeseriesChart/TimeseriesChart.stories.tsx
+++ src/components/TimeseriesChart/TimeseriesChart.stories.tsx
@@ -2,16 +2,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-// import {
-//   datapointsList,
-//   datapoints,
-//   testData,
-// } from 'mocks/timeseriesDataPointsList';
-import { timeseriesList } from '../../mocks/timeseriesList';
 import * as sdk from '@cognite/sdk';
 import TimeseriesChart from './TimeseriesChart';
 
-const randomData = (start: number, end: number, n = 250): sdk.Datapoint[] => {
+const randomData = (start: number, end: number, n: number): sdk.Datapoint[] => {
   const data = [];
   const dt = (end - start) / n;
   for (let i = start; i <= end; i += dt) {
@@ -20,7 +14,7 @@ const randomData = (start: number, end: number, n = 250): sdk.Datapoint[] => {
         () =>
           Math.sin(i / 20) * 50 +
           Math.cos(Math.PI - i / 40) * 50 +
-          Math.random() * 100
+          Math.random() * 40
       )
       .sort((a: number, b: number) => a - b);
     data.push({
@@ -34,14 +28,10 @@ const randomData = (start: number, end: number, n = 250): sdk.Datapoint[] => {
   return data;
 };
 
-const setupMocks = () => {
+const setupMocks = (n = 100) => {
   sdk.TimeSeries.retrieve = async (id: number, _): Promise<sdk.Timeseries> => {
     action('sdk.TimeSeries.retrieve')(id);
-    const timeserie = timeseriesList.find(ts => ts.id === id);
-    if (!timeserie) {
-      throw new Error('Cannot find mocked timeseries');
-    }
-    return timeserie;
+    return { id, name: 'name' };
   };
 
   sdk.Datapoints.retrieve = async (
@@ -49,12 +39,36 @@ const setupMocks = () => {
     params?: sdk.DatapointsRetrieveParams | undefined
   ): Promise<sdk.DataDatapoints> => {
     action('sdk.Datapoints.retrieve')(id, params);
-    const n =
-      params.granularity === 's'
-        ? 2
-        : params.granularity.includes('s')
-        ? 10
-        : 250;
+    return {
+      name: 'name',
+      datapoints: randomData(
+        params ? params.start || 0 : 0,
+        params ? params.end || 100 : 100,
+        n
+      ),
+    };
+  };
+};
+
+const setupZoomableMocks = () => {
+  sdk.TimeSeries.retrieve = async (id: number, _): Promise<sdk.Timeseries> => {
+    action('sdk.TimeSeries.retrieve')(id);
+    return { id, name: 'name' };
+  };
+
+  sdk.Datapoints.retrieve = async (
+    id: number,
+    params?: sdk.DatapointsRetrieveParams | undefined
+  ): Promise<sdk.DataDatapoints> => {
+    action('sdk.Datapoints.retrieve')(id, params);
+    if (params === undefined) {
+      return {
+        name: 'name',
+        datapoints: randomData(0, 100, 250),
+      };
+    }
+    const granularity = params.granularity || '10s';
+    const n = granularity === 's' ? 2 : granularity.includes('s') ? 10 : 250;
     return {
       name: 'name',
       // datapoints,
@@ -67,67 +81,45 @@ const setupMocks = () => {
   };
 };
 
-// const ApiKey = 'ZTg3NzA4ZmMtNWExYS00OWMzLWExZmItZmRiNGFlOTc0MGM3';
-
-// sdk.configure({
-//   apiKey: ApiKey,
-//   project: 'publicdata',
-// });
-
 storiesOf('TimeseriesChart', module)
   .addDecorator(story => <div style={{ width: '100%' }}>{story()}</div>)
+  .add('Empty', () => {
+    setupMocks();
+    return <TimeseriesChart timeseriesIds={[]} />;
+  })
   .add('Single', () => {
     setupMocks();
-    return (
-      <TimeseriesChart
-        timeseriesIds={[timeseriesList[0].id]}
-        start={+Date.now() - 30 * 24 * 60 * 60 * 1000}
-        end={+Date.now()}
-      />
-    );
+    return <TimeseriesChart timeseriesIds={[123]} />;
   })
   .add('Multiple', () => {
     setupMocks();
-    return (
-      <TimeseriesChart
-        timeseriesIds={[timeseriesList[0].id, timeseriesList[1].id]}
-        start={+Date.now() - 30 * 24 * 60 * 60 * 1000}
-        end={+Date.now()}
-      />
-    );
+    return <TimeseriesChart timeseriesIds={[123, 456]} />;
   })
   .add('Context chart', () => {
     setupMocks();
+    return <TimeseriesChart timeseriesIds={[123]} contextChart={true} />;
+  })
+  .add('Zoomable', () => {
+    setupZoomableMocks();
     return (
       <TimeseriesChart
-        timeseriesIds={[timeseriesList[0].id]}
+        timeseriesIds={[123]}
         start={+Date.now() - 30 * 24 * 60 * 60 * 1000}
         end={+Date.now()}
+        zoomable={true}
         contextChart={true}
       />
     );
   })
   .add('Live update', () => {
-    setupMocks();
+    setupMocks(1);
     return (
       <TimeseriesChart
-        timeseriesIds={[timeseriesList[0].id]}
-        start={+Date.now() - 30 * 24 * 60 * 60 * 1000}
+        timeseriesIds={[123]}
+        start={+Date.now() - 60 * 1000}
         end={+Date.now()}
         liveUpdate={true}
         updateIntervalMillis={2000}
-      />
-    );
-  })
-  .add('Zoomable', () => {
-    setupMocks();
-    return (
-      <TimeseriesChart
-        timeseriesIds={[timeseriesList[0].id]}
-        start={+Date.now() - 30 * 24 * 60 * 60 * 1000}
-        end={+Date.now()}
-        zoomable={true}
-        contextChart={true}
       />
     );
   });
--- src/components/TimeseriesChart/TimeseriesChart.test.tsx
+++ src/components/TimeseriesChart/TimeseriesChart.test.tsx
@@ -1,23 +1,69 @@
 import React from 'react';
-import { mount, configure } from 'enzyme';
+import { configure, mount } from 'enzyme';
 import * as sdk from '@cognite/sdk';
 import Adapter from 'enzyme-adapter-react-16';
-import TimeseriesChart from 'components/TimeseriesChart/TimeseriesChart';
-
-const ApiKey = 'ZTg3NzA4ZmMtNWExYS00OWMzLWExZmItZmRiNGFlOTc0MGM3';
-
-sdk.configure({
-  apiKey: ApiKey,
-  project: 'publicdata',
-});
+import TimeseriesChart from './TimeseriesChart';
+import { timeseriesList, datapointsList } from '../../mocks';
 
 configure({ adapter: new Adapter() });
 
+sdk.TimeSeries.retrieve = jest.fn();
+sdk.Datapoints.retrieve = jest.fn();
+
+beforeEach(() => {
+  // @ts-ignore
+  sdk.TimeSeries.retrieve.mockResolvedValue(timeseriesList[0]);
+  // @ts-ignore
+  sdk.Datapoints.retrieve.mockResolvedValue(datapointsList);
+});
+
 // tslint:disable:no-big-function
 describe('TimeseriesChart', () => {
-  it('Renders without exploding', () => {
-    const props = {};
+  it('renders correctly', done => {
+    const props = {
+      timeseriesIds: [timeseriesList[0].id],
+    };
     const wrapper = mount(<TimeseriesChart {...props} />);
-    expect(wrapper.exists()).toBe(true);
+    setImmediate(() => {
+      wrapper.update();
+      expect(wrapper.find('.linechart-container').exists()).toBeTruthy();
+      jest.clearAllMocks();
+      done();
+    });
+  });
+
+  it('calls the sdk', done => {
+    const id = 123;
+    const props = {
+      timeseriesIds: [id],
+    };
+    const wrapper = mount(<TimeseriesChart {...props} />);
+    setImmediate(() => {
+      wrapper.update();
+      expect(sdk.TimeSeries.retrieve).toHaveBeenCalledTimes(1);
+      expect(sdk.TimeSeries.retrieve).toHaveBeenCalledWith(id);
+      expect(sdk.Datapoints.retrieve).toHaveBeenCalledTimes(1);
+      expect(sdk.Datapoints.retrieve).toHaveBeenCalledWith(
+        id,
+        expect.anything()
+      );
+      jest.clearAllMocks();
+      done();
+    });
+  });
+
+  it('renders context chart', done => {
+    const props = {
+      timeseriesIds: [timeseriesList[0].id],
+      contextChart: true,
+    };
+    const wrapper = mount(<TimeseriesChart {...props} />);
+    // tslint:disable-next-line: no-identical-functions
+    setImmediate(() => {
+      wrapper.update();
+      expect(wrapper.find('.context-container').exists()).toBeTruthy();
+      jest.clearAllMocks();
+      done();
+    });
   });
 });
