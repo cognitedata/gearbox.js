40e938f3cfd027bea47593a3dd886c4b8c62c77b
Alexander Cherepnya <cherepnya.alexander@gmail.com>
2019-04-25 10:32:33 +0300

    more tests for AssetMeta

--- src/components/AssetMeta/AssetMeta.test.tsx
+++ src/components/AssetMeta/AssetMeta.test.tsx
@@ -47,4 +47,44 @@ describe('AssetMeta', () => {
       done();
     });
   });
+
+  it('should render "no asset" if assetId was not passed', () => {
+    // @ts-ignore
+    const wrapper = mount(<AssetMeta />);
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.find(AssetMeta)).toHaveLength(1);
+    expect(wrapper.find('p')).toHaveLength(1);
+    expect(wrapper.find('p').text()).toEqual('no Asset');
+  });
+
+  it('should fetch asset, events and documents if assetId was passed after creation', done => {
+    // @ts-ignore
+    const wrapper = mount(<AssetMeta />);
+    wrapper.setProps({ assetId: 123 }, () => {
+      setImmediate(() => {
+        wrapper.update();
+        expect(wrapper.find(AssetMeta)).toHaveLength(1);
+        expect(wrapper.find('h3').text()).toEqual(ASSET_DATA.name);
+        expect(wrapper.find('h3 + p').text()).toEqual(ASSET_DATA.description);
+        done();
+      });
+    });
+  });
+
+  it('should render spinner while loading asset, events and documents', () => {
+    // @ts-ignore
+    sdk.Assets.retrieve.mockImplementation(
+      (): Promise<sdk.Asset> => {
+        return new Promise(resolve => {
+          setTimeout(() => {
+            resolve(ASSET_DATA);
+          }, 10000);
+        });
+      }
+    );
+
+    const wrapper = mount(<AssetMeta assetId={123} />);
+    expect(wrapper.find('Spin')).toHaveLength(1);
+    expect(wrapper.find('div.ant-spin.ant-spin-spinning')).toHaveLength(1);
+  });
 });
