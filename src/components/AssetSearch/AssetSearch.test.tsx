254afe6318d95e558c3e24ac5b3c4260bc7118f6
Torbjørn Follan <torbjorn.follan@itera.no>
2019-05-03 12:33:21 +0200

    Add more tests to asset search

--- src/components/AssetSearch/AssetSearch.test.tsx
+++ src/components/AssetSearch/AssetSearch.test.tsx
@@ -1,6 +1,10 @@
+import * as sdk from '@cognite/sdk';
+import { Input } from 'antd';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import lodash from 'lodash';
 import React from 'react';
+import { assetsList } from '../../mocks';
 import { AssetSearch } from './AssetSearch';
 import Mock = jest.Mock;
 
@@ -11,10 +15,23 @@ const propsCallbacks: { [name: string]: Mock } = {
   onLiveSearchSelect: jest.fn(),
 };
 
+// ignore debounce
+jest.spyOn(lodash, 'debounce').mockImplementation((f: any) => {
+  return f;
+});
+
+sdk.Assets.search = jest.fn();
+beforeEach(() => {
+  // @ts-ignore
+  sdk.Assets.search.mockResolvedValue({ items: assetsList });
+});
+
 afterEach(() => {
   Object.keys(propsCallbacks).forEach((key: string) =>
     propsCallbacks[key].mockClear()
   );
+  // @ts-ignore
+  sdk.Assets.search.mockClear();
 });
 
 describe('AssetSearch', () => {
@@ -23,5 +40,47 @@ describe('AssetSearch', () => {
     const props = { onLiveSearchSelect };
     const wrapper = mount(<AssetSearch {...props} />);
     expect(wrapper.exists()).toBe(true);
+  });
+
+  it('should search when input changes', done => {
+    const { onLiveSearchSelect } = propsCallbacks;
+    const props = { onLiveSearchSelect };
+    const wrapper = mount(<AssetSearch {...props} />);
+
+    wrapper
+      .find(Input)
+      .find('input')
+      .simulate('change', { target: { value: 'value' } });
+
+    setImmediate(() => {
+      wrapper.update();
+      // @ts-ignore
+      wrapper
+        .find('li')
+        .first()
+        .props()
+        .onMouseDown();
+      expect(onLiveSearchSelect).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  it('should call onError when error', done => {
+    // @ts-ignore
+    sdk.Assets.search.mockRejectedValue(new Error('error'));
+
+    const { onLiveSearchSelect, onError } = propsCallbacks;
+    const props = { onLiveSearchSelect, onError };
+    const wrapper = mount(<AssetSearch {...props} />);
+
+    wrapper
+      .find(Input)
+      .find('input')
+      .simulate('change', { target: { value: 'value' } });
+    setImmediate(() => {
+      wrapper.update();
+      expect(onError).toHaveBeenCalled();
+      done();
+    });
   });
 });
