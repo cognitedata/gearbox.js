abebf4edfe0cb49f265a79ccc3e6e645c97b2b1c
Alexander Cherepnya <cherepnya.alexander@gmail.com>
2019-05-17 15:55:30 +0300

    rollup chunks

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,40 @@
+import fs from 'fs';
 import typescript from 'rollup-plugin-typescript2';
 import json from 'rollup-plugin-json';
 import pkg from './package.json';
+
+const exportedComponents = fs
+  .readdirSync('src/components')
+  .filter(dir => dir !== 'common' && dir !== '_internal');
 
 export default {
   external: [
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
   ],
-  input: 'src/index.ts',
+  input: {
+    index: 'src/index.ts',
+    ...exportedComponents.reduce((acc, component) => {
+      acc[`components/${component}/index`] = `src/components/${component}/index.ts`;
+      return acc;
+    }, {}),
+  },
   output: [
     {
-      file: pkg.main,
+      dir: 'dist',
+      entryFileNames: '[name].js',
       format: 'cjs',
     },
     {
-      file: pkg.module,
+      dir: 'dist',
+      entryFileNames: '[name].es.js',
       format: 'es',
     },
   ],
   plugins: [
     typescript({
       typescript: require('typescript'),
+      exclude: [ "**/*.(test|stories).ts+(|x)" ],
     }),
     json(),
   ],
