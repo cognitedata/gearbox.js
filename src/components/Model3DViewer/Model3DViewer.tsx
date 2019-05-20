5bd74237ed94dc39c3fc9d5dbedfd01d29843ff1
Anders Hafreager <anders.hafreager@cognite.com>
2019-05-20 23:01:35 +0200

    Enabled keyboard navigation by default

--- src/components/Model3DViewer/Model3DViewer.tsx
+++ src/components/Model3DViewer/Model3DViewer.tsx
@@ -34,7 +34,7 @@ export function mockCreateViewer(mockFunction: any) {
 
 export class Model3DViewer extends React.Component<Model3DViewerProps> {
   static defaultProps = {
-    enableKeyboardNavigation: false,
+    enableKeyboardNavigation: true,
     useDefaultCameraPosition: true,
   };
 
