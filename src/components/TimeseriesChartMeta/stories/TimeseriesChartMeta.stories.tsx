3c49733e12d8592da21550722fcf89f6baeb4371
Alexander Cherepnya <cherepnya.alexander@gmail.com>
2019-05-31 18:31:57 +0300

    full description for TimeseriesChartMeta

--- src/components/TimeseriesChartMeta/stories/TimeseriesChartMeta.stories.tsx
+++ src/components/TimeseriesChartMeta/stories/TimeseriesChartMeta.stories.tsx
@@ -5,17 +5,35 @@ import { timeseriesList } from '../../../mocks/timeseriesList';
 import { setupMocks } from '../../TimeseriesChart/stories/TimeseriesChart.stories';
 import { TimeseriesChartMeta } from '../TimeseriesChartMeta';
 
+import full from './full.md';
+
 const timeseries = timeseriesList[0];
 
 storiesOf('TimeseriesChartMeta', module).add(
   'Full description',
   () => {
     setupMocks();
-    return <TimeseriesChartMeta timeseries={timeseries} />;
+    const ts = {
+      id: 8681821313339919,
+      name: 'IA_21PT1019.AlarmByte',
+      isString: false,
+      unit: 'bar',
+      metadata: {
+        tag: 'IA_21PT1019.AlarmByte',
+        scan: '1',
+        span: '100',
+        step: '1',
+        zero: '0',
+      },
+      assetId: 4965555138606429,
+      isStep: false,
+      description: '21PT1019.AlarmByte',
+    };
+    return <TimeseriesChartMeta timeseries={ts} />;
   },
   {
     readme: {
-      content: '',
+      content: full,
     },
   }
 );
