b60a5f966bbf3a6f654ab134612a82ef5b29ca9d
Alexander Cherepnya <cherepnya.alexander@gmail.com>
2019-06-04 10:09:33 +0300

    export types for TimeseriesChartMeta

--- src/components/TimeseriesChartMeta/TimeseriesChartMeta.tsx
+++ src/components/TimeseriesChartMeta/TimeseriesChartMeta.tsx
@@ -1,8 +1,12 @@
 import { withTimeseries } from '../../hoc/withTimeseries/withTimeseries';
 import {
+  TimeseriesChartMetaPeriod,
   TimeseriesChartMetaProps,
   TimeseriesChartMetaPure,
 } from './TimeseriesChartMetaPure';
+
+export type TimeseriesChartMetaProps = TimeseriesChartMetaProps;
+export type TimeseriesChartMetaPeriod = TimeseriesChartMetaPeriod;
 
 export const TimeseriesChartMeta = withTimeseries<TimeseriesChartMetaProps>(
   TimeseriesChartMetaPure
