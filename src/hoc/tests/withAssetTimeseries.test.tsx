bc7e040a5ad63a4a82cf7a04321d3b26649110f4
Alexander Cherepnya <cherepnya.alexander@gmail.com>
2019-06-04 18:30:23 +0300

    HOC withAssetTimeseries

--- src/hoc/tests/withAssetTimeseries.test.tsx
+++ src/hoc/tests/withAssetTimeseries.test.tsx
@@ -2,27 +2,30 @@ import * as sdk from '@cognite/sdk';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import { EVENTS } from '../../mocks';
-import { withAssetEvents, WithAssetEventsDataProps } from '../withAssetEvents';
+import { timeseriesList } from '../../mocks';
+import {
+  withAssetTimeseries,
+  WithAssetTimeseriesDataProps,
+} from '../withAssetTimeseries';
 
 configure({ adapter: new Adapter() });
 
-sdk.Events.list = jest.fn();
+sdk.TimeSeries.list = jest.fn();
 
-describe('withAssetEvents', () => {
+describe('withAssetTimeseries', () => {
   beforeEach(() => {
     // @ts-ignore
-    sdk.Events.list.mockResolvedValue({ items: EVENTS });
+    sdk.TimeSeries.list.mockResolvedValue({ items: timeseriesList });
   });
 
   afterEach(() => {
     // @ts-ignore
-    sdk.Events.list.mockClear();
+    sdk.TimeSeries.list.mockClear();
   });
 
   it('Should render spinner', () => {
     const TestComponent = () => <div>Test Content</div>;
-    const WrappedComponent = withAssetEvents(TestComponent);
+    const WrappedComponent = withAssetTimeseries(TestComponent);
     const wrapper = mount(<WrappedComponent assetId={123} />);
 
     expect(wrapper.find('span.ant-spin-dot.ant-spin-dot-spin')).toHaveLength(1);
@@ -31,9 +34,11 @@ describe('withAssetEvents', () => {
 
   it('Should call render spinner', done => {
     const TestComponent = () => <div>Test Content</div>;
-    const WrappedComponent = withAssetEvents(TestComponent);
+    const WrappedComponent = withAssetTimeseries(TestComponent);
     const loadHandler = jest.fn();
-    mount(<WrappedComponent assetId={123} onAssetEventsLoaded={loadHandler} />);
+    mount(
+      <WrappedComponent assetId={123} onAssetTimeseriesLoaded={loadHandler} />
+    );
 
     setImmediate(() => {
       expect(loadHandler).toBeCalled();
@@ -42,24 +47,24 @@ describe('withAssetEvents', () => {
   });
 
   it('Wrapped component should receive asset events  after loading', done => {
-    const TestComponent: React.SFC<WithAssetEventsDataProps> = props => (
+    const TestComponent: React.SFC<WithAssetTimeseriesDataProps> = props => (
       <div>
-        <p className="events-number">{props.assetEvents.length}</p>
-        <p className="first-event-description">
-          {props.assetEvents[0].description}
+        <p className="ts-number">{props.assetTimeseries.length}</p>
+        <p className="first-ts-description">
+          {props.assetTimeseries[0].description}
         </p>
       </div>
     );
-    const WrappedComponent = withAssetEvents(TestComponent);
+    const WrappedComponent = withAssetTimeseries(TestComponent);
     const wrapper = mount(<WrappedComponent assetId={123} />);
 
     setImmediate(() => {
       wrapper.update();
-      expect(wrapper.find('p.events-number').text()).toEqual(
-        EVENTS.length.toString()
+      expect(wrapper.find('p.ts-number').text()).toEqual(
+        timeseriesList.length.toString()
       );
-      expect(wrapper.find('p.first-event-description').text()).toEqual(
-        EVENTS[0].description
+      expect(wrapper.find('p.first-ts-description').text()).toEqual(
+        timeseriesList[0].description
       );
       done();
     });
@@ -67,20 +72,20 @@ describe('withAssetEvents', () => {
 
   it('Should request for an asset if assetId has been changed', done => {
     const TestComponent = () => <div />;
-    const WrappedComponent = withAssetEvents(TestComponent);
+    const WrappedComponent = withAssetTimeseries(TestComponent);
     const wrapper = mount(<WrappedComponent assetId={123} />);
 
     wrapper.setProps({ assetId: 1234 });
 
     setImmediate(() => {
-      expect(sdk.Events.list).toBeCalledTimes(2);
+      expect(sdk.TimeSeries.list).toBeCalledTimes(2);
       done();
     });
   });
 
   it('Should not call setState on unmounted component', done => {
     const TestComponent = () => <div />;
-    const WrappedComponent = withAssetEvents(TestComponent);
+    const WrappedComponent = withAssetTimeseries(TestComponent);
     WrappedComponent.prototype.setState = jest.fn();
     const wrapper = mount(<WrappedComponent assetId={123} />);
 
---
+++ src/hoc/withAssetTimeseries.tsx
@@ -0,0 +1,127 @@
+import { Timeseries } from '@cognite/sdk';
+import * as sdk from '@cognite/sdk';
+import React from 'react';
+import styled from 'styled-components';
+import { Subtract } from 'utility-types';
+import { LoadingOverlay } from '../components/common/LoadingOverlay/LoadingOverlay';
+import {
+  CanceledPromiseException,
+  ComponentWithUnmountState,
+  connectPromiseToUnmountState,
+} from '../utils/promise';
+
+export interface WithAssetTimeseriesDataProps {
+  assetTimeseries: Timeseries[];
+}
+
+export interface WithAssetProps {
+  assetId: number;
+  onAssetTimeseriesLoaded?: (assetTimeseries: Timeseries[]) => void;
+}
+
+export interface WithAssetTimeseriesState {
+  isLoading: boolean;
+  assetTimeseries: Timeseries[] | null;
+  assetId: number;
+}
+
+export const withAssetTimeseries = <P extends WithAssetTimeseriesDataProps>(
+  WrapperComponent: React.ComponentType<P>
+) =>
+  class
+    extends React.Component<
+      Subtract<P, WithAssetTimeseriesDataProps> & WithAssetProps,
+      WithAssetTimeseriesState
+    >
+    implements ComponentWithUnmountState {
+    static getDerivedStateFromProps(
+      props: P & WithAssetProps,
+      state: WithAssetTimeseriesState
+    ) {
+      if (props.assetId !== state.assetId) {
+        return {
+          isLoading: true,
+          assetTimeseries: null,
+          assetId: props.assetId,
+        };
+      }
+
+      return null;
+    }
+
+    isComponentUnmounted = false;
+
+    constructor(props: P & WithAssetProps) {
+      super(props);
+
+      this.state = {
+        isLoading: true,
+        assetTimeseries: null,
+        assetId: props.assetId,
+      };
+    }
+
+    componentDidMount() {
+      this.loadAssetTimeseries();
+    }
+
+    componentWillUnmount() {
+      this.isComponentUnmounted = true;
+    }
+
+    componentDidUpdate(prevProps: P & WithAssetProps) {
+      if (prevProps.assetId !== this.props.assetId) {
+        this.loadAssetTimeseries();
+      }
+    }
+
+    async loadAssetTimeseries() {
+      try {
+        const res = await connectPromiseToUnmountState(
+          this,
+          sdk.TimeSeries.list({ assetId: this.props.assetId, limit: 1000 })
+        );
+
+        this.setState({
+          isLoading: false,
+          assetTimeseries: res.items,
+        });
+
+        if (this.props.onAssetTimeseriesLoaded) {
+          this.props.onAssetTimeseriesLoaded(res.items);
+        }
+      } catch (error) {
+        if (error instanceof CanceledPromiseException !== true) {
+          throw error;
+        }
+      }
+    }
+
+    render() {
+      const { isLoading, assetTimeseries } = this.state;
+
+      if (isLoading) {
+        return (
+          <SpinnerContainer>
+            <LoadingOverlay isLoading={true} backgroundColor={'none'} />
+          </SpinnerContainer>
+        );
+      }
+
+      if (assetTimeseries) {
+        return (
+          <WrapperComponent
+            {...(this.props as any) as P}
+            assetTimeseries={assetTimeseries}
+          />
+        );
+      }
+
+      return null;
+    }
+  };
+
+const SpinnerContainer = styled.div`
+  position: relative;
+  height: 300px;
+`;
