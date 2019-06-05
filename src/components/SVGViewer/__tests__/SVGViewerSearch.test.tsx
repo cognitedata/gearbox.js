5f06873a7fc09d2b1caafca9321f5ddac807fc44
Torbjørn Follan <torbjorn.follan@itera.no>
2019-06-05 15:00:26 +0200

    Add some tests to SVGViewerSearch

--- src/components/SVGViewer/__tests__/SVGViewerSearch.test.tsx
+++ src/components/SVGViewer/__tests__/SVGViewerSearch.test.tsx
@@ -5,12 +5,21 @@ import SVGViewerSearch from '../SVGViewerSearch';
 
 configure({ adapter: new Adapter() });
 
+const propsCallBacks = {
+  addCssClassesToMetadataContainer: jest.fn(),
+  addCssClassesToSvgText: jest.fn(),
+};
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+const svgSource = 'http://www.w3.org/2000/svg';
 describe('SVGViewerSearch', () => {
   it('Renders without exploding', () => {
     const wrapper = mount(
       <SVGViewerSearch
         visible={true}
-        svg={document.createElementNS('http://www.w3.org/2000/svg', 'svg')}
+        svg={document.createElementNS(svgSource, 'svg')}
         isDesktop={true}
         openSearch={() => null}
         zoomOnCurrentAsset={() => null}
@@ -22,5 +31,57 @@ describe('SVGViewerSearch', () => {
       />
     );
     expect(wrapper).toHaveLength(1);
+  });
+
+  it('should add css classes to metadata container', () => {
+    const { addCssClassesToMetadataContainer } = propsCallBacks;
+    const wrapper = mount(
+      <SVGViewerSearch
+        visible={true}
+        svg={document.createElementNS(svgSource, 'svg')}
+        isDesktop={true}
+        openSearch={() => null}
+        zoomOnCurrentAsset={() => null}
+        handleCancelSearch={() => null}
+        addCssClassesToMetadataContainer={addCssClassesToMetadataContainer}
+        addCssClassesToSvgText={() => null}
+        onFocus={() => null}
+        onBlur={() => null}
+      />
+    );
+    // @ts-ignore
+    wrapper
+      .find('input')
+      .first()
+      .props()
+      // @ts-ignore
+      .onChange({ currentTarget: { value: 'svg' } });
+    expect(addCssClassesToMetadataContainer).toHaveBeenCalled();
+  });
+
+  it('should add css classes to svg text', () => {
+    const { addCssClassesToSvgText } = propsCallBacks;
+    const wrapper = mount(
+      <SVGViewerSearch
+        visible={true}
+        svg={document.createElementNS(svgSource, 'svg')}
+        isDesktop={true}
+        openSearch={() => null}
+        zoomOnCurrentAsset={() => null}
+        handleCancelSearch={() => null}
+        addCssClassesToMetadataContainer={() => null}
+        addCssClassesToSvgText={addCssClassesToSvgText}
+        onFocus={() => null}
+        onBlur={() => null}
+      />
+    );
+    // @ts-ignore
+    wrapper
+      .find('input')
+      .first()
+      .props()
+      // @ts-ignore
+      .onChange({ currentTarget: { value: 'svg' } });
+    expect(addCssClassesToSvgText).toHaveBeenCalled();
   });
 });
