8e9d55e45bf46b16d418c539885d007b1c3fbaca
Alexander Cherepnya <cherepnya.alexander@gmail.com>
2019-06-14 11:09:09 +0300

    #332: updated HOC withAssetFiles to SDK 2.0

--- src/hoc/tests/withAssetFiles.test.tsx
+++ src/hoc/tests/withAssetFiles.test.tsx
@@ -1,30 +1,42 @@
-import * as sdk from '@cognite/sdk';
+import { API } from '@cognite/sdk-alpha/dist/src/resources/api';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
+import { ClientSDKProvider } from '../../components/ClientSDKProvider';
 import { SDK_LIST_LIMIT } from '../../constants/sdk';
-import { DOCUMENTS } from '../../mocks';
+import { fakeFiles } from '../../mocks';
 import { withAssetFiles, WithAssetFilesDataProps } from '../withAssetFiles';
 
 configure({ adapter: new Adapter() });
 
-sdk.Files.list = jest.fn();
+const fakeClient: API = {
+  // @ts-ignore
+  files: {
+    list: jest.fn(),
+  },
+};
 
 describe('withAssetFiles', () => {
   beforeEach(() => {
     // @ts-ignore
-    sdk.Files.list.mockResolvedValue({ items: DOCUMENTS });
+    fakeClient.files.list.mockReturnValue({
+      autoPagingToArray: () => Promise.resolve(fakeFiles),
+    });
   });
 
   afterEach(() => {
     // @ts-ignore
-    sdk.Files.list.mockClear();
+    fakeClient.files.list.mockClear();
   });
 
   it('Should render spinner', () => {
     const TestComponent = () => <div>Test Content</div>;
     const WrappedComponent = withAssetFiles(TestComponent);
-    const wrapper = mount(<WrappedComponent assetId={123} />);
+    const wrapper = mount(
+      <ClientSDKProvider client={fakeClient}>
+        <WrappedComponent assetId={123} />
+      </ClientSDKProvider>
+    );
 
     expect(wrapper.find('span.ant-spin-dot.ant-spin-dot-spin')).toHaveLength(1);
     wrapper.unmount();
@@ -34,10 +46,12 @@ describe('withAssetFiles', () => {
     const TestComponent = () => <div>Test Content</div>;
     const WrappedComponent = withAssetFiles(TestComponent);
     const wrapper = mount(
-      <WrappedComponent
-        assetId={123}
-        customSpinner={<div className="my-custom-spinner" />}
-      />
+      <ClientSDKProvider client={fakeClient}>
+        <WrappedComponent
+          assetId={123}
+          customSpinner={<div className="my-custom-spinner" />}
+        />
+      </ClientSDKProvider>
     );
     expect(wrapper.find('div.my-custom-spinner')).toHaveLength(1);
   });
@@ -46,7 +60,11 @@ describe('withAssetFiles', () => {
     const TestComponent = () => <div>Test Content</div>;
     const WrappedComponent = withAssetFiles(TestComponent);
     const loadHandler = jest.fn();
-    mount(<WrappedComponent assetId={123} onAssetFilesLoaded={loadHandler} />);
+    mount(
+      <ClientSDKProvider client={fakeClient}>
+        <WrappedComponent assetId={123} onAssetFilesLoaded={loadHandler} />
+      </ClientSDKProvider>
+    );
 
     setImmediate(() => {
       expect(loadHandler).toBeCalled();
@@ -54,37 +72,47 @@ describe('withAssetFiles', () => {
     });
   });
 
-  it('Wrapped component should receive asset files  after loading', done => {
+  it('Wrapped component should receive asset files after loading', done => {
     const TestComponent: React.SFC<WithAssetFilesDataProps> = props => (
       <div>
         <p className="files-number">{props.assetFiles.length}</p>
-        <p className="first-file-name">{props.assetFiles[0].fileName}</p>
+        <p className="first-file-name">{props.assetFiles[0].name}</p>
       </div>
     );
     const WrappedComponent = withAssetFiles(TestComponent);
-    const wrapper = mount(<WrappedComponent assetId={123} />);
+    const wrapper = mount(
+      <ClientSDKProvider client={fakeClient}>
+        <WrappedComponent assetId={123} />
+      </ClientSDKProvider>
+    );
 
     setImmediate(() => {
       wrapper.update();
       expect(wrapper.find('p.files-number').text()).toEqual(
-        DOCUMENTS.length.toString()
+        fakeFiles.length.toString()
       );
       expect(wrapper.find('p.first-file-name').text()).toEqual(
-        DOCUMENTS[0].fileName
+        fakeFiles[0].name
       );
       done();
     });
   });
 
-  it('Should request for an asset if assetId has been changed', done => {
+  it('Should request for list of files if assetId has been changed', done => {
     const TestComponent = () => <div />;
     const WrappedComponent = withAssetFiles(TestComponent);
-    const wrapper = mount(<WrappedComponent assetId={123} />);
+    const wrapper = mount(
+      <ClientSDKProvider client={fakeClient}>
+        <WrappedComponent assetId={123} />
+      </ClientSDKProvider>
+    );
 
-    wrapper.setProps({ assetId: 1234 });
+    wrapper.setProps({
+      children: <WrappedComponent assetId={1234} />,
+    });
 
     setImmediate(() => {
-      expect(sdk.Files.list).toBeCalledTimes(2);
+      expect(fakeClient.files.list).toBeCalledTimes(2);
       done();
     });
   });
@@ -93,7 +121,11 @@ describe('withAssetFiles', () => {
     const TestComponent = () => <div />;
     const WrappedComponent = withAssetFiles(TestComponent);
     WrappedComponent.prototype.setState = jest.fn();
-    const wrapper = mount(<WrappedComponent assetId={123} />);
+    const wrapper = mount(
+      <ClientSDKProvider client={fakeClient}>
+        <WrappedComponent assetId={123} />
+      </ClientSDKProvider>
+    );
 
     wrapper.unmount();
 
@@ -106,26 +138,34 @@ describe('withAssetFiles', () => {
   it('Should merge query params with assetId', () => {
     const WrappedComponent = withAssetFiles(() => <div />);
     mount(
-      <WrappedComponent
-        assetId={123}
-        queryParams={{ assetId: 34234, limit: 78, sort: 'sort option' }}
-      />
+      <ClientSDKProvider client={fakeClient}>
+        <WrappedComponent
+          assetId={123}
+          queryParams={{
+            limit: 78,
+            filter: { assetIds: [34234], name: 'test name' },
+          }}
+        />
+      </ClientSDKProvider>
     );
 
-    expect(sdk.Files.list).toBeCalledWith({
-      assetId: 123,
+    expect(fakeClient.files.list).toBeCalledWith({
       limit: 78,
-      sort: 'sort option',
+      filter: { assetIds: [123], name: 'test name' },
     });
   });
 
-  it('Should call sdk.Events.list with default limit', () => {
+  it('Should call sdkClient.files.list with default limit', () => {
     const WrappedComponent = withAssetFiles(() => <div />);
-    mount(<WrappedComponent assetId={123} />);
+    mount(
+      <ClientSDKProvider client={fakeClient}>
+        <WrappedComponent assetId={123} />\
+      </ClientSDKProvider>
+    );
 
-    expect(sdk.Files.list).toBeCalledWith({
-      assetId: 123,
+    expect(fakeClient.files.list).toBeCalledWith({
       limit: SDK_LIST_LIMIT,
+      filter: { assetIds: [123] },
     });
   });
 });
