a651e0150058de0a2667b98f1ce2976ac3f8f952
Alexander Cherepnya <cherepnya.alexander@gmail.com>
2019-06-18 16:03:38 +0300

    api.ts - removed unused functions

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,8 @@
 import {
   Asset,
   Assets,
-  Event,
-  Events,
-  File,
-  Files,
   Revision,
   ThreeD,
-  TimeSeries,
-  Timeseries,
 } from '@cognite/sdk';
 import { ApiAssetList } from '../interfaces';
 
@@ -37,50 +31,4 @@ export function fetch3DModelRevision(
   revisionId: number
 ): Promise<Revision> {
   return ThreeD.retrieveRevision(modelId, revisionId);
-}
-
-export async function retrieveAsset(assetId: number) {
-  return await Assets.retrieve(assetId);
-}
-
-export async function getAssetListDescendants(
-  assetId: number,
-  query: { [name: string]: any }
-) {
-  const response = await Assets.listDescendants(assetId, query);
-
-  if (!response.items || response.items.length < 1) {
-    return [];
-  }
-
-  return response.items;
-}
-
-export async function getAssetEvent(query: {
-  assetId: number;
-  limit: number;
-}): Promise<Event[]> {
-  const response = await Events.list(query);
-
-  if (response.items && response.items.length > 0) {
-    return response.items;
-  }
-
-  return [];
-}
-
-export async function getAssetFiles(query: {
-  assetId: number;
-  limit: number;
-}): Promise<File[]> {
-  const response = await Files.list(query);
-  return response.items;
-}
-
-export async function getAssetTimeseries(query: {
-  assetId: number;
-  limit: number;
-}): Promise<Timeseries[]> {
-  const response = await TimeSeries.list(query);
-  return response.items;
 }
