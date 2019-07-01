18c50eec2a68c1d29e323de105e7920aaa3f1361
Valentyn Dembitskyi <joyrostie@gmail.com>
2019-07-01 11:43:44 +0300

    Joyrostie/sdk2.0 asset search issue#360 (#366)
    * Metadata empty string fix
    
    * Hide liveSearchShow with advancedSearchQuery

--- src/components/AssetSearch/AssetSearch.tsx
+++ src/components/AssetSearch/AssetSearch.tsx
@@ -147,7 +147,7 @@ export class AssetSearch extends React.Component<
               (query.advancedSearch &&
                 query.advancedSearch.metadata &&
                 query.advancedSearch.metadata.reduce(
-                  (a, c) => ({ ...a, [c.key]: c.value }),
+                  (a, c) => (c.key ? { ...a, [c.key]: c.value } : a),
                   {}
                 )) ||
               undefined,
