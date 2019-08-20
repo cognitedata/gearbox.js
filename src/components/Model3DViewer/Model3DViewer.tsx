9cfd97af4825f20e356948e5b28014484ee6668a
David Liu <david.liu@cognite.com>
2019-08-20 16:16:13 +0200

    Updated based on comments

--- src/components/Model3DViewer/Model3DViewer.tsx
+++ src/components/Model3DViewer/Model3DViewer.tsx
@@ -295,7 +295,9 @@ export class Model3DViewer extends React.Component<Model3DViewerProps> {
     }
   };
 
-  /** Return a slicing plane for a specified axis. */
+  /**
+   * @return a slicing plane for a specified axis
+   */
   sliceByAxis = (sliceDetail: SlicingDetail, axisNumber: number) => {
     const vector = [0, 0, 0];
     vector[axisNumber] = sliceDetail.direction ? 1 : -1;
@@ -305,7 +307,9 @@ export class Model3DViewer extends React.Component<Model3DViewerProps> {
     );
   };
 
-  /** Slice the model based on the slicing properties passed in. */
+  /**
+   * Slices the model based on the slicing properties passed in.
+   */
   slice = (sliceProps: SlicingProps) => {
     if (this.viewer) {
       const planes = this.planes;
@@ -326,7 +330,9 @@ export class Model3DViewer extends React.Component<Model3DViewerProps> {
     }
   };
 
-  /** Update the slicing planes as user drag the sliders. */
+  /**
+   * Update the slicing planes as user drag the sliders.
+   */
   onChange = (val: number, axisNumber: number) => {
     this.planes[axisNumber].set(
       this.planes[axisNumber].normal,
@@ -337,7 +343,9 @@ export class Model3DViewer extends React.Component<Model3DViewerProps> {
     }
   };
 
-  /** Reverse the direction of the slicing axis. */
+  /**
+   * Reverse the direction of the slicing axis.
+   */
   flipSlider = (axisNumber: number) => {
     this.planes[axisNumber].negate();
     this.flipped[axisNumber] = !this.flipped[axisNumber];
@@ -346,7 +354,9 @@ export class Model3DViewer extends React.Component<Model3DViewerProps> {
     }
   };
 
-  /** Render a single slider. */
+  /**
+   * Render a single slider.
+   */
   renderSlider = (range: SliderRange | undefined, axisNumber: number) => {
     if (!range) {
       return null;
@@ -378,7 +388,9 @@ export class Model3DViewer extends React.Component<Model3DViewerProps> {
     );
   };
 
-  /** Render all sliders. */
+  /**
+   * Render all sliders.
+   */
   renderSliders = () => {
     if (!this.props.slider) {
       return null;
