8cb794f97306f617d11227107f1b9e05da4517ee
amem <amem.vck@gmail.com>
2019-11-20 12:09:25 +0530

    docs: add stories to detailsPanel without props

---
+++ src/components/AssetDetailsPanel/stories/helper.tsx
@@ -0,0 +1,48 @@
+import { Asset } from '@cognite/sdk';
+import React from 'react';
+import {
+  ASSET_LIST_CHILD,
+  ASSET_ZERO_DEPTH_ARRAY,
+} from '../../../mocks/assetsListV2';
+import { MockCogniteClient } from '../../../utils/mockSdk';
+import { ClientSDKProvider } from '../../ClientSDKProvider';
+
+class CogniteClient extends MockCogniteClient {
+  assets: any = {
+    retrieve: (ids: { id: number }[]) =>
+      new Promise(resolve => {
+        setTimeout(() => {
+          const allAssets = [...ASSET_ZERO_DEPTH_ARRAY, ...ASSET_LIST_CHILD];
+          const result = ids.map(({ id }) => {
+            return allAssets.find(asset => asset.id === id);
+          });
+
+          resolve(result || []);
+        }, 200);
+      }),
+  };
+}
+
+export const Sdk = ( obj:any ) =>{
+  const {children} = obj
+  console.log(obj);
+  return <ClientSDKProvider client={client}>{children}</ClientSDKProvider> 
+}
+
+const client = new CogniteClient({ appId: 'gearbox test' });
+
+export const decorators = [
+  (storyFn: any) => (
+    <ClientSDKProvider client={client}>{storyFn()}</ClientSDKProvider>
+  ),
+];
+
+export const customElementRendering = (
+  asset: Asset,
+  depth: number
+): JSX.Element => (
+  <span style={{ backgroundColor: 'red' }}>{`${depth}. ${asset.name ||
+    'undefined'}`}</span>
+);
+
+export const handleAssetLoaded = (asset: Asset) => { console.log(asset)};
