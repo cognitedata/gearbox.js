4dcb47a9b12cfc8c1dbefba28c2be351083acc36
Asanka <amem.vck@gmail.com>
2019-11-22 13:56:33 +0530

    docs(story): AssetDetailsPanel stories in new format

---
+++ src/components/AssetDetailsPanel/stories/helper.tsx
@@ -0,0 +1,34 @@
+import { Asset } from '@cognite/sdk';
+import React from 'react';
+import { fakeAsset } from '../../../mocks';
+import { MockCogniteClient } from '../../../utils/mockSdk';
+import { ClientSDKProvider } from '../../ClientSDKProvider';
+
+class CogniteClient extends MockCogniteClient {
+  assets: any = {
+    retrieve: () =>
+      new Promise(resolve => {
+        setTimeout(() => {
+          resolve([fakeAsset]);
+        }, 1000);
+      }),
+  };
+}
+
+const client = new CogniteClient({ appId: 'gearbox test' });
+
+export const decorators = [
+  (storyFn: any) => (
+    <ClientSDKProvider client={client}>{storyFn()}</ClientSDKProvider>
+  ),
+];
+
+export const customStyle: React.CSSProperties = {
+  border: '1px solid red',
+};
+
+export const toCategory = (name: string): string => name.split('_')[0];
+
+export const onAssetLoaded = (asset: Asset) => {
+  console.log(asset);
+};
