4494dad00a3356843f286b1e1d6a3bb59285052c
Asanka <amem.vck@gmail.com>
2019-12-05 16:18:14 +0530

    docs(asset-document-panel): stories in new format (#524)
    * docs(story): asset documentpanel in new format
    
    * docs: comments resolve
    
    * docs(story): comments resolve

---
+++ src/components/AssetDocumentsPanel/stories/helper.tsx
@@ -0,0 +1,39 @@
+import { FilesMetadata } from '@cognite/sdk';
+import React from 'react';
+import { AssetDocumentsPanelStyles } from '../../../interfaces';
+import { DOCUMENTS, sleep } from '../../../mocks';
+import { MockCogniteClient } from '../../../utils/mockSdk';
+import { ClientSDKProvider } from '../../ClientSDKProvider';
+class CogniteClient extends MockCogniteClient {
+  files: any = {
+    list: () => ({
+      autoPagingToArray: () => {
+        return new Promise(async resolve => {
+          await sleep(1000);
+          resolve(DOCUMENTS);
+        });
+      },
+    }),
+  };
+}
+
+const client = new CogniteClient({ appId: 'gearbox test' });
+
+export const decorators = [
+  (storyFn: any) => (
+    <ClientSDKProvider client={client}>{storyFn()}</ClientSDKProvider>
+  ),
+];
+
+export const handleAssetFilesLoaded = (files: FilesMetadata[]) => {
+  console.log(files);
+};
+
+export const docTypes = { XB: 'My Category', XL: 'Another Custom Category' };
+
+export const customStyle: AssetDocumentsPanelStyles = {
+  wrapper: { border: '1px solid red' },
+  fileContainer: { backgroundColor: '#DDD', padding: 8 },
+  fileLink: { color: 'purple' },
+  fileTitle: { color: 'magenta', fontSize: '1em' },
+};
