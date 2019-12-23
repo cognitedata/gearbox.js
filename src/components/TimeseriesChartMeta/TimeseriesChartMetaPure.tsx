bfefc2d192e4b82a89d45744b16dddf732fbe800
Asanka <amem.vck@gmail.com>
2019-12-23 22:16:39 +0530

    docs(time-series-chart-meta): rewrite stories in new format (#558)

--- src/components/TimeseriesChartMeta/TimeseriesChartMetaPure.tsx
+++ src/components/TimeseriesChartMeta/TimeseriesChartMetaPure.tsx
@@ -1,7 +1,6 @@
-import { Radio } from 'antd';
-import { RadioChangeEvent } from 'antd/lib/radio';
+import Radio, { RadioChangeEvent } from 'antd/lib/radio';
 import moment from 'moment';
-import React from 'react';
+import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 import { WithTimeseriesDataProps } from '../../hoc/withTimeseries';
 import { TimeseriesChart } from '../TimeseriesChart';
@@ -42,17 +41,44 @@ const timeScales: { [key: string]: { unit: string; number: number } } = {
 export type TimeseriesChartMetaPeriod = keyof typeof timeScales;
 
 export interface TimeseriesChartMetaProps extends WithTimeseriesDataProps {
+  /**
+   * Defines whether to get live updates in chart and data point
+   */
   liveUpdate?: boolean;
+  /**
+   * Interval in milliseconds for live updates
+   */
   updateIntervalMillis?: number;
+  /**
+   *  One of six predefined time periods: `'lastYear'`, `'lastMonth'`, `'lastWeek'`, `'lastDay'`, `'lastHour'`, `'last15Minutes'`
+   */
   defaultTimePeriod?: TimeseriesChartMetaPeriod;
+  /**
+   * Custom time period for TimeseriesChart. This prop overrides defaultTimePeriod. Time values should be in timestamp format.
+   */
   defaultBasePeriod?: {
     startTime: number;
     endTime: number;
   };
+  /**
+   * Defines whether to show description of the timeseries
+   */
   showDescription?: boolean;
+  /**
+   * Defines whether to show time periods radio buttons
+   */
   showPeriods?: boolean;
+  /**
+   * Defines whether to show timeseries chart
+   */
   showChart?: boolean;
+  /**
+   * Defines whether to show current data point (sensor value)
+   */
   showDatapoint?: boolean;
+  /**
+   * Defines whether to show meta data of the timeseries
+   */
   showMetadata?: boolean;
 }
 
@@ -64,7 +90,7 @@ interface TimeseriesChartMetaState {
   };
 }
 
-export class TimeseriesChartMetaPure extends React.PureComponent<
+export class TimeseriesChartMetaPure extends PureComponent<
   TimeseriesChartMetaProps,
   TimeseriesChartMetaState
 > {
---
+++ src/components/TimeseriesChartMeta/stories/helper.tsx
@@ -0,0 +1,74 @@
+import { GetTimeSeriesMetadataDTO } from '@cognite/sdk';
+import React from 'react';
+import { sleep } from '../../../mocks';
+import { ClientSDKProvider } from '../../ClientSDKProvider';
+import {
+  MockDatapointsClientObject,
+  TimeseriesMockClient,
+} from '../../TimeseriesChart/stories/TimeseriesChart.stories';
+
+class CogniteClient extends TimeseriesMockClient {
+  timeseries: any = {
+    retrieve: async (): Promise<GetTimeSeriesMetadataDTO[]> => {
+      await sleep(1000);
+      return [
+        {
+          id: 8681821313339919,
+          name: 'IA_21PT1019.AlarmByte',
+          isString: false,
+          unit: 'bar',
+          metadata: {
+            tag: 'IA_21PT1019.AlarmByte',
+            scan: '1',
+            span: '100',
+            step: '1',
+            zero: '0',
+          },
+          assetId: 4965555138606429,
+          isStep: false,
+          description: '21PT1019.AlarmByte',
+          createdTime: new Date(0),
+          lastUpdatedTime: new Date(0),
+        },
+      ];
+    },
+  };
+  datapoints: any = {
+    ...MockDatapointsClientObject,
+    retrieveLatest: async () => {
+      await sleep(1000);
+      return [
+        {
+          isString: false,
+          id: 123,
+          datapoints: [
+            {
+              timestamp: new Date(Date.now()),
+              value: 15 + Math.random() * 5.0,
+            },
+          ],
+        },
+      ];
+    },
+  };
+}
+
+const client = new CogniteClient({ appId: 'gearbox test' });
+
+export const decorators = [
+  (storyFn: any) => (
+    <ClientSDKProvider client={client}>{storyFn()}</ClientSDKProvider>
+  ),
+];
+
+export const defaultBasePeriodProps = {
+  startTime: Date.now() - 10 * 24 * 60 * 60 * 1000, // 10 days ago
+  endTime: Date.now(),
+};
+
+export const exampleTheme = {
+  gearbox: {
+    textColor: 'Red',
+    textColorSecondary: 'Coral',
+  },
+};
