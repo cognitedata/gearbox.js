20a7c1c86a137f5072a19d57d23777e86df0b91b
Asanka <amem.vck@gmail.com>
2019-12-24 17:55:02 +0530

    docs(asset-scanner): rewrite stories in new format (#548)

--- src/components/AssetScanner/AssetScanner.tsx
+++ src/components/AssetScanner/AssetScanner.tsx
@@ -1,5 +1,5 @@
 import { Asset, CogniteClient } from '@cognite/sdk';
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 import { ocrRecognize } from '../../api';
@@ -60,24 +60,83 @@ export interface AssetScannerStyles {
 }
 
 export interface AssetScannerProps {
+  /**
+   * Function that provides custom OCR call to detect strings on image
+   */
   ocrRequest: (ocrParams: OcrRequest) => Promise<string[]>;
+  /**
+   * Base64 image representation which uses as a source for recognition
+   * (instead of webcamera image)
+   */
   image?: string;
+  /**
+   * ocrKey	API key property which is needed if you use embedded Google Vision API
+   */
   ocrKey?: string;
+  /**
+   * Render prop function, that should returns button node. Capture function and
+   * image base64 string are passed as arguments
+   */
   button?: ButtonRenderProp;
+  /**
+   * Function that gets a result from the recognize function and formats it as an
+   * array of recognized strings (Can be used if you use embedded Google Vision API)
+   */
   extractOcrStrings?: (ocrResult: any) => string[];
+  /**
+   * enableNotification	Flag that controls enabling/disabling notification feature.
+   * If customNotification doesn't set then antd messages will be used by default
+   */
   enableNotification?: boolean;
+  /**
+   * Callback function to react on provided type of notification (will be ignored if enableNotification = false)
+   */
   customNotification?: ASNotification;
+  /**
+   * Callback triggered when image recognition process starts
+   */
   onImageRecognizeStart?: (image: string) => void;
+  /**
+   * Callback triggered when image recognition process is finished and right before fetching assets by recognized strings
+   */
   onImageRecognizeFinish?: (strings: string[] | null) => void;
+  /**
+   * Callback triggered if strings haven't been recognized on provided image
+   */
   onImageRecognizeEmpty?: Callback;
+  /**
+   * Callback triggered when SDK asset search has been finished
+   */
   onAssetFetchResult?: OnAssetListCallback;
+  /**
+   * Callback triggered right after taking a shot from camera
+   */
   onStartLoading?: EmptyCallback;
+  /**
+   * Callback triggered after finishing recognition process
+   */
   onEndLoading?: EmptyCallback;
+  /**
+   * Callback triggered when occurs an error related to OCR service/request
+   */
   onOcrError?: Callback;
+  /**
+   * Callback triggered when an error occurs
+   */
   onError?: Callback;
+  /**
+   * Callback triggered after resetting captured image
+   */
   onImageReset?: Callback;
-  imageAltText?: string;
+  /**
+   * styles	Object that defines inline CSS styles for inner elements of the component
+   * (Use if you not provide button render prop)
+   */
   styles?: AssetScannerStyles;
+  /**
+   * Object that defines strings to be passed to component
+   */
+  imageAltText?: string;
   strings?: PureObject;
   cropSize?: CropSize;
   webcamCropOverlay?: React.ComponentType;
@@ -90,7 +149,7 @@ interface AssetScannerState {
   imageSrc: string;
 }
 
-export class AssetScanner extends React.Component<
+export class AssetScanner extends Component<
   AssetScannerProps,
   AssetScannerState
 > {
---
+++ src/components/AssetScanner/stories/helper.tsx
@@ -0,0 +1,111 @@
+import { message } from 'antd';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Callback, ErrorResponse } from '../../../interfaces';
+import { assetsList, sleep } from '../../../mocks';
+import { MockCogniteClient } from '../../../utils/mockSdk';
+import { ClientSDKProvider } from '../../ClientSDKProvider';
+import { ASNotifyTypes } from '../AssetScanner';
+
+class CogniteClient extends MockCogniteClient {
+  assets: any = {
+    list: () => ({
+      autoPagingToArray: async () => {
+        await sleep(1000);
+        // @ts-ignore
+        return assetsList;
+      },
+    }),
+  };
+}
+
+const client = new CogniteClient({ appId: 'gearbox test' });
+
+export const decorators = [
+  (storyFn: any) => (
+    <ClientSDKProvider client={client}>{storyFn()}</ClientSDKProvider>
+  ),
+];
+
+export const FileInputComponent = ({ render }: any) => {
+  const [image, setImage] = useState('');
+
+  const onChange = async (e: any) => {
+    const file = e.target.files[0];
+
+    if (!file) {
+      return;
+    }
+
+    const fr = new FileReader();
+    fr.readAsDataURL(file);
+
+    const uploadedImage = await new Promise(resolve => {
+      fr.onloadend = res => {
+        if (res) {
+          resolve((res.currentTarget as any).result);
+        }
+      };
+    });
+
+    setImage(uploadedImage as any);
+  };
+
+  return (
+    <>
+      {render(image)}
+      <p style={{ margin: '10px' }}>
+        <input type="file" accept={'image/png'} onChange={onChange} />
+      </p>
+    </>
+  );
+};
+
+export const ocrRequest = async ({ image }: { image: string }) => {
+  console.log(image.slice(0, 10) + '...');
+  return ['result'];
+};
+
+export const onError = (error: any) => {
+  console.log('onError', error);
+};
+
+export const renderButton = (
+  capture: Callback,
+  isReady: boolean = true
+): React.ReactNode => {
+  const Button = styled('button')`
+    border-radius: 10px;
+    height: 50px;
+    background-color: red;
+    position: absolute;
+    top: 50%;
+    left: 30px;
+    transform: translateY(-50%);
+  `;
+  return <Button onClick={capture}>{!isReady ? 'Reset' : 'Capture'}</Button>;
+};
+
+export const customNotification = (type: ASNotifyTypes) => message.info(type);
+
+export const onOcrError = (error: ErrorResponse) => {
+  console.log('onOcrError', error);
+};
+
+export const onImageRecognizeFinish = (result: string[] | null) => {
+  console.log('onImageRecognizeFinish', result);
+};
+
+export const cropSize = { width: 400, height: 200 };
+
+export const getAssetHandlerCustom = async () => [];
+
+export const webcamCropOverlay = () => (
+  <div
+    style={{
+      border: '20px solid red',
+      height: '240px',
+      width: '440px',
+    }}
+  />
+);
