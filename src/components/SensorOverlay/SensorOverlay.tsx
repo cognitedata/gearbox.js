183384df2a858eb18d964d5e6d5685b3cce70c96
Asanka <amem.vck@gmail.com>
2019-12-24 19:11:55 +0530

    docs(sensor-overlay): rewrite stories in new format (#563)

--- src/components/SensorOverlay/SensorOverlay.tsx
+++ src/components/SensorOverlay/SensorOverlay.tsx
@@ -1,5 +1,5 @@
 import { omit, sortedIndex } from 'lodash';
-import React, { Component } from 'react';
+import React, { Component, ReactNode } from 'react';
 import {
   ConnectDropTarget,
   DragDropContext,
@@ -87,38 +87,94 @@ interface DraggableBoxPosition extends SensorPosition {
 }
 
 export interface SensorOverlayProps {
-  children: React.ReactNode;
+  /**
+   * List of timeserie Ids
+   */
   timeseriesIds: number[];
-  alertColor?: string;
+  /**
+   * Wrapped content. Usually infographic image
+   */
+  children: ReactNode;
+  /**
+   * Number in milliseconds that defines refresh interval for fetching latest timeserie data
+   */
+  refreshInterval?: number;
+  /**
+   * Object map that defines custom colors for timeseries
+   */
   colorMap: {
     [id: string]: string;
   };
+  /**
+   * Object map that defines position of newly added sensors in timeseriesIds.
+   * The map doesn't affect position of previously added or dragged sensors.
+   */
   defaultPositionMap: {
     [id: string]: SensorPosition;
   };
+  /**
+   * Object map that defines if it's needed to wrap timeserie values in the anchor tag <a>,
+   * works in conjunction with onLinkClick
+   */
   linksMap: {
     [id: string]: boolean;
   };
+  /**
+   * Object map that defines which timeseries will show tooltips with name and description without mouse hovering
+   */
   stickyMap: {
     [id: string]: boolean;
   };
+  /**
+   * Object map that defines a normal range for sensor values and once the value is out of this range an alert bar will be shown
+   */
   minMaxMap: {
     [id: string]: SensorMinMaxRange;
   };
+  /**
+   * Works in conjuction with minMaxMap.
+   * Defines background color of the alert tooltip that is shown if sensor value is out of min/max range.
+   * The dragdable circle in the tag should also have the alert color if the sensor value is out of range.
+   */
+  alertColor?: string;
+  /**
+   * Defines whether it's possible to drag sensor boxes (tags)
+   */
   isTagDraggable: boolean;
+  /**
+   * Defines whether it's possible to drag sensor pointers
+   */
   isPointerDraggable: boolean;
-  onSensorPositionChange?: (id: number, position: SensorPosition) => void;
-  onSettingsClick?: (id: number) => void;
+  /**
+   * Function triggered when user clicks on a sensor box or pointer
+   */
   onClick?: (id: number) => void;
+  /**
+   * Function triggered when user clicks on the settings icon. The icon is shown if this prop is defined.
+   */
+  onSettingsClick?: (id: number) => void;
+  /**
+   * Function triggered when user clicks on a sensor value link. The link should be enabled with linksMap
+   */
   onLinkClick?: (id: number, dataPoint?: Datapoint) => void;
   connectDropTarget: ConnectDropTarget;
+  /**
+   * Function triggered when either a tag or a pointer has been dragged.
+   */
+  onSensorPositionChange?: (id: number, position: SensorPosition) => void;
+  /**
+   * By default SensorOverlay takes 100% width in current block context
+   * but if fixedWidth is given the width will be fixed by the number in pixels
+   */
+  fixedWidth?: number;
+  /**
+   * Object map with strings to customize/localize text in the component
+   */
+  strings?: { [key: string]: string };
   size: {
     width: number;
     height: number;
   };
-  fixedWidth?: number;
-  refreshInterval?: number;
-  strings?: { [key: string]: string };
 }
 
 interface SensorOverlayState {
@@ -432,3 +488,5 @@ const FinalSensorOverlay = DragDropContext(HTML5Backend)(SizeWrapper);
 FinalSensorOverlay.displayName = 'SensorOverlay';
 
 export { FinalSensorOverlay as SensorOverlay };
+
+export { SensorOverlay as SensorOverlayWithoutCustomize };
---
+++ src/components/SensorOverlay/stories/helper.tsx
@@ -0,0 +1,125 @@
+import { GetTimeSeriesMetadataDTO } from '@cognite/sdk';
+import React from 'react';
+import { sleep, timeseriesListV2 as fakeTimeseries } from '../../../mocks';
+import { MockCogniteClient } from '../../../utils/mockSdk';
+import { ClientSDKProvider } from '../../ClientSDKProvider';
+import { SensorDatapoint, SensorOverlay } from '../SensorOverlay';
+
+class CogniteClient extends MockCogniteClient {
+  timeseries: any = {
+    retrieve: async (ids: any): Promise<GetTimeSeriesMetadataDTO[]> => {
+      await sleep(Math.random() * 2000);
+      // @ts-ignore
+      const timeserie = fakeTimeseries.find(ts => ts.id === ids[0].id);
+      if (!timeserie) {
+        throw new Error('Cannot find mocked timeseries');
+      }
+      return [timeserie];
+    },
+  };
+  datapoints: any = {
+    retrieveLatest: async (_: any) => {
+      return [
+        {
+          id: 1,
+          isString: false,
+          datapoints: [
+            {
+              timestamp: new Date(),
+              value: Math.random() * 5.0,
+            },
+          ],
+        },
+      ];
+    },
+  };
+}
+
+// tslint:disable-next-line:max-classes-per-file
+export class SensorOverlayWrapperComponent extends React.Component {
+  state = {
+    counter: 0,
+    timeseriesIds: [],
+  };
+
+  render() {
+    return (
+      <div>
+        <button
+          style={{ marginBottom: 20 }}
+          onClick={() =>
+            this.setState({
+              timeseriesIds: [
+                ...this.state.timeseriesIds,
+                fakeTimeseries[this.state.counter].id,
+              ],
+              counter: this.state.counter + 1,
+            })
+          }
+        >
+          Add Sensor
+        </button>
+        <SensorOverlay timeseriesIds={this.state.timeseriesIds}>
+          <div
+            style={{
+              width: '100%',
+              height: '300px',
+              background: '#EEE',
+            }}
+          />
+        </SensorOverlay>
+      </div>
+    );
+  }
+}
+
+const client = new CogniteClient({ appId: 'gearbox test' });
+
+export const decorators = [
+  (storyFn: any) => (
+    <ClientSDKProvider client={client}>{storyFn()}</ClientSDKProvider>
+  ),
+];
+
+export const CustomizedDiv = () => (
+  <div style={{ width: '100%', height: '220px', background: '#EEE' }} />
+);
+
+export const timeseriesIds = [
+  8681821313339919,
+  4965555138606429,
+  1762612637163055,
+  7108578362782757,
+];
+
+export const colorMap = { [fakeTimeseries[0].id]: '#33AA33' };
+
+export const defaultPositionMap = {
+  [fakeTimeseries[0].id]: {
+    left: 0.5,
+    top: 0.2,
+    pointer: {
+      left: 0.4,
+      top: 0.8,
+    },
+  },
+};
+
+export const linksMap = { [fakeTimeseries[0].id]: true };
+
+export const handleClick = (timeserieId: number) =>
+  console.log('handleClick', timeserieId);
+
+export const handleLinkClick = (
+  timeserieId: number,
+  datapoint: SensorDatapoint
+) => console.log('handleLinkClick', timeserieId, datapoint);
+
+export const minMaxMap = {
+  [8681821313339919]: {
+    min: 5,
+    max: 10,
+  },
+};
+
+export const stickyMap = { [8681821313339919]: true };
