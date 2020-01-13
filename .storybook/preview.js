94338a3755d3a2da20436f0e837d71478ed5952a
Roman Vizniak <mr.traser@gmail.com>
2020-01-13 16:28:48 +0200

    chore: clean up storybook packages

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,27 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
-import { addDecorator, addParameters } from '@storybook/react';
-import { addReadme, configureReadme } from 'storybook-readme';
-import { DocsContainer, Preview } from '@storybook/addon-docs/dist/blocks';
-import { SourceState } from '@storybook/addon-docs/dist/blocks';
-
-const StoryWrapper = styled.div`
-  margin: 1em;
-`;
-
-const DocWrapper = styled.div`
-  margin: 1em;
-  table {
-    display: table!important;
-  }
-`;
-
-configureReadme({
-  DocPreview: ({children}) => <DocWrapper>{children}</DocWrapper>,
-  StoryPreview: ({children}) => <StoryWrapper>{children}</StoryWrapper>,
-});
-
-addDecorator(addReadme);
+import { addParameters } from '@storybook/react';
+import { DocsContainer, Preview, SourceState } from '@storybook/addon-docs/dist/blocks';
 
 addParameters({
   docs: {
