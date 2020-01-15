d72f27bce88fcae03613db62500479154aeb740c
Roman Vizniak <mr.traser@gmail.com>
2020-01-15 13:39:28 +0200

    docs: build storybook with canvas and docs tab

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { addParameters } from '@storybook/react';
+import styled from 'styled-components';
+import { addParameters, addDecorator } from '@storybook/react';
 import { DocsContainer, Preview, SourceState } from '@storybook/addon-docs/dist/blocks';
+
+const Decorator = (fn) => (
+  <Wrapper>
+    <Content>
+      { fn() }
+    </Content>
+  </Wrapper>
+);
+
+addDecorator(Decorator);
 
 addParameters({
   options: {
-    showRoots: true
+    showRoots: true,
+    selectedPanel: 'docs',
   },
   docs: {
     container: DocsContainer,
     preview: Preview.defaultProps = { withSource: SourceState.OPEN }
   },
 });
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 4rem 20px;
+`;
+
+const Content = styled.div`
+  max-width: 1000px;
+  width: 100%;
+`;
