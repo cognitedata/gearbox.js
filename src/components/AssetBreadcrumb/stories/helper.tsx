2d3eba6836ea6d166a8a7c1ed9bd0e0612a023f3
Rumesh Wickramasinghe <rumesh.wickramasinghe@gmail.com>
2020-01-17 14:54:20 +0530

    docs(asset-breadcrumb): add custom assets fetching (#596)
    * docs(asset-breadcrumb): add custom assets fetching
    
    * docs(asset-breadcrumb): replace settimeout with await sleep()
    
    * docs(asset-breadcrumb): replace promise with await sleep()
    
    * docs(asset-breadcrumb): fix lint lissues

--- src/components/AssetBreadcrumb/stories/helper.tsx
+++ src/components/AssetBreadcrumb/stories/helper.tsx
@@ -1,26 +1,24 @@
-import { Asset } from '@cognite/sdk';
+import { Asset, CogniteInternalId } from '@cognite/sdk';
 import React, { FC } from 'react';
 import {
   ASSET_LIST_CHILD,
   ASSET_ZERO_DEPTH_ARRAY,
   MockCogniteClient,
+  sleep,
 } from '../../../mocks';
 import { ClientSDKProvider } from '../../ClientSDKProvider';
 import { AssetBreadcrumbProps } from '../interfaces';
 
 class CogniteClient extends MockCogniteClient {
   assets: any = {
-    retrieve: (ids: { id: number }[]) =>
-      new Promise(resolve => {
-        setTimeout(() => {
-          const allAssets = [...ASSET_ZERO_DEPTH_ARRAY, ...ASSET_LIST_CHILD];
-          const result = ids.map(({ id }) => {
-            return allAssets.find(asset => asset.id === id);
-          });
-
-          resolve(result || []);
-        }, 200);
-      }),
+    retrieve: async (ids: { id: number }[]) => {
+      await sleep(200);
+      const allAssets = [...ASSET_ZERO_DEPTH_ARRAY, ...ASSET_LIST_CHILD];
+      const result = ids.map(({ id }) => {
+        return allAssets.find(asset => asset.id === id);
+      });
+      return result || [];
+    },
   };
 }
 
@@ -42,5 +40,10 @@ export const customElementRendering = (
 
 export const onBreadcrumbClick = (asset: Asset, depth: number) =>
   console.log(asset, depth);
+
+export const retrieveAsset = async (assetId: CogniteInternalId) => {
+  await sleep(100);
+  return ASSET_LIST_CHILD.find(asset => asset.id === assetId);
+};
 
 export const ComponentProps: FC<AssetBreadcrumbProps> = () => <></>;
