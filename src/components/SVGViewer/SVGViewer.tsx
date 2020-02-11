ff6e0469a458c6fd2d13421a24007e038e01d252
David Liu <david.liu@cognite.com>
2020-02-10 22:45:11 -0500

    feat: new svg viewer implementation

--- src/components/SVGViewer/SVGViewer.tsx
+++ src/components/SVGViewer/SVGViewer.tsx
@@ -2,7 +2,16 @@ import { CogniteClient } from '@cognite/sdk';
 import { Icon } from 'antd';
 import PinchZoom from 'pinch-zoom-js';
 import React, { Component, KeyboardEvent, RefObject } from 'react';
+import {
+  ALIGN_COVER,
+  fitSelection,
+  fitToViewer,
+  INITIAL_VALUE,
+  ReactSVGPanZoom,
+} from 'react-svg-pan-zoom';
+import { AutoSizer } from 'react-virtualized';
 import styled from 'styled-components';
+import * as transformationMatrix from 'transformation-matrix';
 import { ERROR_NO_SDK_CLIENT } from '../../constants/errorMessages';
 import { ClientSDKProxyContext } from '../../context/clientSDKProxyContext';
 import * as CustomIcon from './Icons';
@@ -27,6 +36,8 @@ interface SvgViewerState {
   isSearchFocused: boolean;
   width: number;
   handleKeyDown: boolean;
+  svg?: SVGSVGElement;
+  value: any;
 }
 
 export class SVGViewer extends Component<SvgViewerProps, SvgViewerState> {
@@ -36,11 +47,14 @@ export class SVGViewer extends Component<SvgViewerProps, SvgViewerState> {
   client!: CogniteClient;
   prevMoveDistanceX: number = 0;
   prevMoveDistanceY: number = 0;
-  dragging: boolean = false;
-  startMouse!: ZoomCenter;
+  dragging: boolean;
+  initialLoad: boolean;
+  startMouse: boolean;
   pinchZoomInstance!: PinchZoomInterface;
   inputWrapper: RefObject<HTMLInputElement>;
   svg!: SVGSVGElement;
+  viewer: React.RefObject<ReactSVGPanZoom>;
+  svgNodeRef: React.RefObject<SVGGElement>;
   pinchZoom: React.RefObject<HTMLDivElement>;
   pinchZoomContainer: React.RefObject<HTMLDivElement>;
   svgParentNode: React.RefObject<HTMLDivElement>;
@@ -51,8 +65,14 @@ export class SVGViewer extends Component<SvgViewerProps, SvgViewerState> {
       isSearchFocused: false,
       handleKeyDown: false,
       width: window.innerWidth,
+      value: INITIAL_VALUE,
     };
+    this.dragging = false;
+    this.startMouse = false;
+    this.initialLoad = true;
 
+    this.viewer = React.createRef();
+    this.svgNodeRef = React.createRef();
     this.pinchZoom = React.createRef();
     this.pinchZoomContainer = React.createRef();
     this.svgParentNode = React.createRef();
@@ -70,7 +90,7 @@ export class SVGViewer extends Component<SvgViewerProps, SvgViewerState> {
     window.addEventListener('resize', this.updateWindowDimensions);
   }
 
-  componentDidUpdate(prevProps: SvgViewerProps) {
+  componentDidUpdate(prevProps: SvgViewerProps, prevState: SvgViewerState) {
     const { documentId: prevDocId } = prevProps as SvgViewerDocumentIdProps;
     const { documentId: curDocId } = this.props as SvgViewerDocumentIdProps;
     const { file: prevFile } = prevProps as SvgViewerFileProps;
@@ -78,6 +98,14 @@ export class SVGViewer extends Component<SvgViewerProps, SvgViewerState> {
 
     if (this.svg) {
       this.setCustomClasses();
+    }
+
+    if (
+      this.state.svg !== prevState.svg &&
+      this.state.svg &&
+      this.viewer.current
+    ) {
+      this.zoomOnCurrentAsset();
     }
 
     if (
@@ -93,6 +121,10 @@ export class SVGViewer extends Component<SvgViewerProps, SvgViewerState> {
     window.removeEventListener('resize', this.updateWindowDimensions);
   }
 
+  changeValue = nextValue => {
+    this.setState({ value: nextValue });
+  };
+
   render() {
     const {
       title,
@@ -104,35 +136,79 @@ export class SVGViewer extends Component<SvgViewerProps, SvgViewerState> {
     const hasCloseButton = !!this.props.handleCancel;
 
     return (
-      <SVGViewerContainer
-        onClick={this.onContainerClick}
-        onKeyDown={this.handleKeyDown}
-      >
-        {isDesktop && (
-          <input
-            type="text"
-            onBlur={this.onContainerBlur}
-            onFocus={this.onContainerFocus}
-            ref={this.inputWrapper}
-            style={{
-              opacity: 0,
-              position: 'absolute',
-              pointerEvents: 'none',
-            }}
-          />
-        )}
-        <SvgNode
-          ref={this.svgParentNode}
-          onMouseDown={this.onMouseDown}
-          onMouseMove={this.onMouseMove}
-          onMouseUp={this.onMouseUp}
-          // disable tap to search functionality for Android
-          // https://developers.google.com/web/updates/2015/10/tap-to-search
-          tabIndex={-1}
-          onWheel={this.handleTrackpadZoom}
-          customClassNames={customClassNames}
-          data-test-id="svg-viewer"
-        >
+      <>
+        <SVGViewerContainer>
+          <AutoSizer>
+            {({ width, height }) =>
+              width === 0 || height === 0 ? null : (
+                <ReactSVGPanZoom
+                  width={width}
+                  height={height}
+                  ref={this.viewer}
+                  tool="pan"
+                  value={this.state.value}
+                  onChangeValue={value => this.changeValue(value)}
+                >
+                  {this.state.svg ? (
+                    <svg
+                      width={this.state.svg.width.baseVal.valueAsString}
+                      height={this.state.svg.height.baseVal.valueAsString}
+                    >
+                      <SvgNode
+                        ref={this.svgNodeRef}
+                        customClassNames={customClassNames}
+                        tabIndex={-1}
+                        onTouchStart={this.onMouseDown}
+                        onTouchMove={this.onMouseMove}
+                        onTouchEnd={this.onMouseUp}
+                        onMouseDown={this.onMouseDown}
+                        onMouseMove={this.onMouseMove}
+                        onMouseUp={this.onMouseUp}
+                        dangerouslySetInnerHTML={{
+                          __html: this.state.svg.innerHTML,
+                        }}
+                        style={{ position: 'relative' }}
+                      />
+                    </svg>
+                  ) : (
+                    <svg width={617} height={316}>
+                      <g fillOpacity=".5" strokeWidth="4">
+                        <rect
+                          x="400"
+                          y="40"
+                          width="100"
+                          height="200"
+                          fill="#4286f4"
+                          stroke="#f4f142"
+                        />
+                        <circle
+                          cx="108"
+                          cy="108.5"
+                          r="100"
+                          fill="#0ff"
+                          stroke="#0ff"
+                        />
+                        <circle
+                          cx="180"
+                          cy="209.5"
+                          r="100"
+                          fill="#ff0"
+                          stroke="#ff0"
+                        />
+                        <circle
+                          cx="220"
+                          cy="109.5"
+                          r="100"
+                          fill="#f0f"
+                          stroke="#f0f"
+                        />
+                      </g>
+                    </svg>
+                  )}
+                </ReactSVGPanZoom>
+              )
+            }
+          </AutoSizer>
           {/* show header if it's desktop or search is hidden for mobiles */}
           {(isDesktop || !this.state.isSearchVisible) && (
             <StyledHeaderContainer>
@@ -199,21 +275,6 @@ export class SVGViewer extends Component<SvgViewerProps, SvgViewerState> {
             currentSearchClassName={customClassNames.currentSearchResult}
             handleSearchChange={handleSearchChange}
           />
-          {/* move pinchZoomContainer if search is visible on mobile */}
-          <div
-            ref={this.pinchZoomContainer}
-            style={
-              !isDesktop && this.state.isSearchVisible
-                ? { top: '65px', position: 'relative' }
-                : {}
-            }
-          >
-            <div
-              ref={this.pinchZoom}
-              onTouchStart={this.onTouchStart}
-              onTouchEnd={this.onTouchEnd}
-            />
-          </div>
           {!isDesktop && !this.state.isSearchFocused ? (
             <ModalMobileFooter>
               <MobileSearchButton
@@ -224,8 +285,8 @@ export class SVGViewer extends Component<SvgViewerProps, SvgViewerState> {
               </MobileSearchButton>
             </ModalMobileFooter>
           ) : null}
-        </SvgNode>
-      </SVGViewerContainer>
+        </SVGViewerContainer>
+      </>
     );
   }
 
@@ -263,12 +324,12 @@ export class SVGViewer extends Component<SvgViewerProps, SvgViewerState> {
         this.initPinchToZoom();
         this.initAttributesForMetadataContainer();
         this.setCustomClasses();
-        this.svg.addEventListener('click', this.handleItemClick);
-        this.zoomOnCurrentAsset(
-          document.querySelector(
-            `.${currentAssetClassName || (customClassNames || {}).currentAsset}`
-          )
-        );
+        // this.zoomOnCurrentAsset(
+        //   this.svg.querySelectorAll(
+        //     `.${currentAssetClassName || (customClassNames || {}).currentAsset}`
+        //   )
+        // );
+        this.setState({ svg: this.svg });
       }
     }
   };
@@ -336,47 +397,28 @@ export class SVGViewer extends Component<SvgViewerProps, SvgViewerState> {
     });
   };
 
-  onMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (!this.pinchZoomInstance) {
-      return;
-    }
-    this.dragging = true;
-    this.pinchZoomInstance.stopAnimation();
-    this.startMouse = {
-      x: event.pageX,
-      y: event.pageY,
-    };
+  onMouseDown = (event: React.MouseEvent | React.TouchEvent) => {
+    this.startMouse = true;
   };
 
-  onMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (this.dragging && this.pinchZoomInstance) {
+  onMouseMove = (event: React.MouseEvent | React.TouchEvent) => {
+    if (event.cancelable) {
       event.preventDefault();
-
-      const currentMoveDistanceX = event.pageX - this.startMouse.x;
-      const currentMoveDistanceY = event.pageY - this.startMouse.y;
-
-      this.pinchZoomInstance.offset.x -=
-        currentMoveDistanceX - this.prevMoveDistanceX;
-      this.pinchZoomInstance.offset.y -=
-        currentMoveDistanceY - this.prevMoveDistanceY;
-      this.pinchZoomInstance.offset = this.pinchZoomInstance.sanitizeOffset(
-        this.pinchZoomInstance.offset
-      );
-      this.pinchZoomInstance.update();
-
-      this.prevMoveDistanceX = currentMoveDistanceX;
-      this.prevMoveDistanceY = currentMoveDistanceY;
+    }
+    if (this.startMouse) {
+      this.dragging = true;
     }
   };
 
-  onMouseUp = () => {
-    const isDesktop = this.state.width > minDesktopWidth;
-    if (isDesktop && this.inputWrapper.current) {
-      this.inputWrapper.current.focus();
+  onMouseUp = (event: React.MouseEvent | React.TouchEvent) => {
+    if (event.cancelable) {
+      event.preventDefault();
+    }
+    if (!this.dragging) {
+      this.handleItemClick(event);
     }
     this.dragging = false;
-    this.prevMoveDistanceX = 0;
-    this.prevMoveDistanceY = 0;
+    this.startMouse = false;
   };
 
   // we need to remove and add back optimization as suggested here
@@ -437,7 +479,7 @@ export class SVGViewer extends Component<SvgViewerProps, SvgViewerState> {
   isNullStrokeWidth = (textNode: Element) =>
     textNode.getAttribute('stroke-width') === '0';
 
-  handleItemClick = (e: MouseEvent) => {
+  handleItemClick = (e: React.MouseEvent | React.TouchEvent) => {
     const target = e.target as HTMLElement;
     const parentNode = target.parentNode as HTMLElement;
     if (parentNode && parentNode.classList.contains('metadata-container')) {
@@ -496,33 +538,88 @@ export class SVGViewer extends Component<SvgViewerProps, SvgViewerState> {
     }
   };
 
-  zoomOnCurrentAsset = (currentAsset: Element | null) => {
-    const isDesktop = this.state.width > minDesktopWidth;
-    if (!currentAsset || !this.pinchZoomInstance) {
+  zoomOnCurrentAsset = () => {
+    const currentNodes = Array.prototype.slice.call(
+      document.querySelectorAll(
+        `.${currentAssetClassName ||
+          (this.props.customClassNames || {}).currentAsset}`
+      )
+    );
+    if (!currentNodes || currentNodes.length === 0) {
+      this.setState({
+        value: fitToViewer(this.state.value, ALIGN_COVER, ALIGN_COVER),
+      });
       return;
-    }
-    this.pinchZoomInstance.zoomFactor = isDesktop
-      ? zoomLevel * 3
-      : zoomLevel * 10;
-    // Need to wait until zoom applies to get proper offsets
+    } 
+    const currentAssets = currentNodes.slice(0, currentNodes.length / 2);
     setTimeout(() => {
-      const currentAssetPosition = currentAsset.getBoundingClientRect();
-      if (currentAssetPosition) {
-        this.pinchZoomInstance.offset = {
-          x:
-            currentAssetPosition.left -
-            this.pinchZoomInstance.container.clientWidth / 2 +
-            this.pinchZoomInstance.offset.x,
-          y:
-            currentAssetPosition.top -
-            this.pinchZoomInstance.container.clientHeight / 2 +
-            this.pinchZoomInstance.offset.y,
-        };
-        // Zoom may create invalid position
-        this.pinchZoomInstance.offset = this.pinchZoomInstance.sanitizeOffset(
-          this.pinchZoomInstance.offset
+      const initialAssetPosition = currentAssets[0].getBBox();
+      let top = initialAssetPosition.y;
+      let left = initialAssetPosition.x;
+      let right = initialAssetPosition.x + initialAssetPosition.width;
+      let bottom = initialAssetPosition.y + initialAssetPosition.height;
+      currentAssets.forEach(currentAsset => {
+        console.log(currentAsset.getBBox());
+        const currentAssetPosition = currentAsset.getBBox();
+        top = Math.min(top, currentAssetPosition.y);
+        left = Math.min(left, currentAssetPosition.x);
+        bottom = Math.max(
+          bottom,
+          currentAssetPosition.y + currentAssetPosition.height
         );
-        this.pinchZoomInstance.update();
+        right = Math.max(
+          right,
+          currentAssetPosition.x + currentAssetPosition.width
+        );
+      });
+
+      console.log(left, top, right - left, bottom - top);
+      if (this.viewer && this.viewer.current) {
+        if (
+          this.svgNodeRef.current &&
+          this.svgNodeRef.current.querySelector('g > g')
+        ) {
+          const attribute = (this.svgNodeRef.current.querySelector(
+            'g > g'
+          ) as SVGGElement).attributes.getNamedItem('transform');
+          if (attribute) {
+            const matrixes = transformationMatrix.fromTransformAttribute(
+              attribute.value
+            );
+            const ctm = transformationMatrix.compose(
+              transformationMatrix.fromDefinition(matrixes)
+            );
+            console.log(this.svgNodeRef.current, ctm);
+            let point = { x: left, y: top };
+            point.x = left;
+            point.y = top;
+            point = transformationMatrix.applyToPoint(ctm, point) as {
+              x: number;
+              y: number;
+            };
+            left = point.x;
+            top = point.y;
+
+            point.x = right;
+            point.y = bottom;
+            point = transformationMatrix.applyToPoint(ctm, point) as {
+              x: number;
+              y: number;
+            };
+            right = point.x;
+            bottom = point.y;
+          }
+        }
+        console.log(left, top, right - left, bottom - top);
+        this.setState({
+          value: fitSelection(
+            this.state.value,
+            left,
+            top,
+            right - left,
+            bottom - top
+          ),
+        });
       }
     });
   };
@@ -608,15 +705,13 @@ interface InternalThemedStyledProps {
   customClassNames: CustomClassNames;
 }
 
-const SvgNode = styled.div`
+const SvgNode = styled.g`
   cursor: move;
   overflow: auto;
   height: 100%;
   width: 100%;
   background: #ffffff;
-  > svg {
-    display: block;
-  }
+
   ${(props: InternalThemedStyledProps) =>
     !props.customClassNames.searchResults &&
     // tslint:disable-next-line: no-nested-template-literals
@@ -718,6 +813,7 @@ const StyledHeaderContainer = styled.div`
   background: #fff;
   color: rgba(0, 0, 0, 0.65);
   border-bottom: 1px solid #e8e8e8;
+  z-index: 1000;
 
   @media (min-width: 992px) {
     padding: 5px 5px 5px 24px;
