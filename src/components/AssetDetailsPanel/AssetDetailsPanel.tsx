bd116e1e462927d16bf3d72cdd5650e854610d98
Roman Vizniak <mr.traser@gmail.com>
2020-04-27 13:29:36 +0300

    fix: linting

--- src/components/AssetDetailsPanel/AssetDetailsPanel.tsx
+++ src/components/AssetDetailsPanel/AssetDetailsPanel.tsx
@@ -2,4 +2,6 @@ import { withAsset } from '../../hoc';
 import { AssetDetailsPanelPure } from './AssetDetailsPanelPure';
 import { AssetDetailsPanelPureProps } from './interfaces';
 
-export const AssetDetailsPanel = withAsset<AssetDetailsPanelPureProps>(AssetDetailsPanelPure);
+export const AssetDetailsPanel = withAsset<AssetDetailsPanelPureProps>(
+  AssetDetailsPanelPure
+);
