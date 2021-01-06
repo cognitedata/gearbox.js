f6a84639ecee593e8b6cd7165d450aa62ae0cfbf
Kirien <kirien.eyma@cognite.com>
2021-01-06 16:16:02 +0100

    feat(svg-viewer): downloadable pdf version of file (#692)
    * feat(svg-viewer): downloadable pdf version of file
    
    * fix: better comment

---
+++ src/components/SVGViewer/Icons/Download.tsx
@@ -0,0 +1,22 @@
+// Copyright 2020 Cognite AS
+import React from 'react';
+
+const Download = () => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M9 8.17419V0H7V8.17426L5.17154 6.3458L3.75732 7.76001L6.58575 10.5884L7.99996 12.0027L9.41418 10.5884L12.2426 7.76001L10.8284 6.3458L9 8.17419Z"
+      fill="currentColor"
+    />
+    <path d="M1 14L15 14V16L1 16L1 14Z" fill="currentColor" />
+  </svg>
+);
+
+export default Download;
