071828215b5a3b368b05b14f3a09ba5c856a1c65
Kirien Eyma <kirien.eyma@cognite.com>
2021-01-08 10:49:31 +0100

    fix(svg-viewer): change download to pdf icon

--- src/components/SVGViewer/Icons/index.ts
+++ src/components/SVGViewer/Icons/index.ts
@@ -3,4 +3,4 @@ export { default as ZoomIn } from './ZoomIn';
 export { default as ZoomOut } from './ZoomOut';
 export { default as FindInPage } from './FindInPage';
 export { default as Close } from './Close';
-export { default as Download } from './Download';
+export { default as PDF } from './PDF';
